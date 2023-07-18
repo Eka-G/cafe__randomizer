97ec98b7e8133e779925d5eddd7c0697f7a7a6da
Eka <ekaglazkovaa@yandex.ru>
2023-07-18 21:17:14 +0300

    feat: add rules description

--- app.vue
+++ app.vue
@@ -1,4 +1,7 @@
 <template>
   <NuxtLayout>
+    <v-container class="v-col-12 v-col-sm-10">
+      <CRDescription class="mb-6" />
+    </v-container>
   </NuxtLayout>
 </template>
---
+++ components/CRDescription.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-sheet class="pa-4">
+    <h1 class="mb-4 text-center main-title">
+      Добро пожаловать в Cafe Random!
+    </h1>
+    <p class="text-body-2 text-sm-body-1 text-center font-weight-bold">
+      Если Вы устали от вопроса "куда сходить поесть?", то Cafe Random поможет определиться с выбором. Выберите случаное заведение простым кликом на кнопку "RANDOM CAFE".<br>
+      Не стесняйтесь пробовать несколько раз!<br>
+      Проверить список мест можно через кнопку <v-icon>mdi-rice</v-icon> "Заведения".
+    </p>
+  </v-sheet>
+</template>
+
+<style>
+  @media (max-width: 599px) {
+    .main-title {
+      font-size: 1.5rem;
+    }
+  }
+</style>
